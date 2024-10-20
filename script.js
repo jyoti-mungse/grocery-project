809c5641b6bd824da71e09fd9fc4865759818f61
Jyoti Mungse <jrmungse@gmail.com>
2024-10-20 22:05:25 +0530

    hi

--- script.js
+++ script.js
@@ -15,3 +15,56 @@ let navbar = document.querySelector(".navbar");
 document.querySelector("#menu-btn").onclick = () => {
   navbar.classList.toggle("active");
 };
+
+var Swiper = new Swiper(".product-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    0: {
+      slidesPreView: 1
+    },
+    769: {
+      slidesPreView: 2
+    },
+    1020: {
+      slidesPreView: 3
+    }
+  }
+});
+var Swiper1 = new Swiper(".review-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    0: {
+      slidesPreView: 1
+    },
+    769: {
+      slidesPreView: 2
+    },
+    1020: {
+      slidesPreView: 3
+    }
+  }
+});
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar"
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
